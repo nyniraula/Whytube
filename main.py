23301916ebc8a0b27ae80958f814e09b6aabb78e
nyniraula <nyniraula@gmail.com>
2026-04-07 09:34:02 +0545

    add fetching mechanism

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
 
+opts = {"no_warnings": True, "quiet": True}
+
+
+def get_media_url():
+    while True:
+        try:
+            url = input("Media Url: ").strip()
+            if not url:
+                raise ValueError
+            break
+
+        except ValueError:
+            print("Invalid URL pattern")
+
+    return url
+
+
+def fetch_media_info():
+    with YoutubeDL(opts) as ydl:
+        while True:
+            try:
+                url = get_media_url()
+                media_info = ydl.extract_info(url, download=False)
+                break
+
+            except DownloadError:
+                print("Invalid url, Try again!")
+
+        return media_info
+    
+
+    
